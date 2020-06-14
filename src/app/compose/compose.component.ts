96217840edb732f1225e6ef4e6b73f05d22daac9
Damanveer <damanveer121@gmail.com>
2020-06-14 16:08:14 +0530

    second commit

--- src/app/compose/compose.component.ts
+++ src/app/compose/compose.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComposeComponent implements OnInit {
 
+    // this is a variable
+    partView = 'more';
+    // this is a functino to change the view variable
+    changePartView(des) {
+      this.partView = des;
+    }
+
+    view='';
+    changeView(de){
+      this.view=de;
+    };
+
   constructor() { }
 
   ngOnInit(): void {
